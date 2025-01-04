54abdd59ad9cbfb8ec08d059fbcc86f71a2c735f
IgelChan <IgelChan@kilohertz.info>
2025-01-04 18:20:24 -0500

    Moved layer shifts to GB and HN

--- keyboards/squarish/planka/keymaps/default/keymap.c
+++ keyboards/squarish/planka/keymaps/default/keymap.c
@@ -23,31 +23,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ CTRL │ GUI │ ALT │ FN │ LOWER │ TAB │ SPC │ RISE │ LEFT │ DOWN │ UP │ RIGHT │
      * └──────┴─────┴─────┴────┴───────┴─────┴─────┴──────┴──────┴──────┴────┴───────┘
      */
-    [0] = LAYOUT_ortho_4x12(    // LettersB
-        QK_GESC,  KC_Q,    KC_W,    KC_E,       KC_R,           KC_T,         KC_Y,       KC_U,         KC_I,         KC_O,       KC_P,       KC_BSPC,
-        KC_BSLS,  KC_A,    KC_S,    KC_D,       KC_F,           KC_G,         KC_H,       KC_J,         KC_K,         KC_L,       KC_SCLN,    KC_QUOT,
-        KC_LSFT,  KC_Z,    KC_X,    KC_C,       KC_V,           LT(3,KC_B),   KC_N,       KC_M,         KC_COMM,      KC_DOT,     KC_SLSH,    SH_ENT,
-        KC_LCTL,  KC_LGUI, KC_LALT, TG(2),      SQ_LOWER,       KC_TAB,       KC_SPC,     SQ_RAISE,     KC_LEFT,      KC_DOWN,    KC_UP,      KC_RGHT
+    [0] = LAYOUT_ortho_4x12(    // Letters
+        QK_GESC,  KC_Q,    KC_W,    KC_E,       KC_R,           KC_T,         KC_Y,         KC_U,         KC_I,         KC_O,       KC_P,       KC_BSPC,
+        KC_BSLS,  KC_A,    KC_S,    KC_D,       KC_F,           LT(3,KC_G),   LT(3,KC_H),   KC_J,         KC_K,         KC_L,       KC_SCLN,    KC_QUOT,
+        KC_LSFT,  KC_Z,    KC_X,    KC_C,       KC_V,           LT(2,KC_B),   LT(2,KC_N),   KC_M,         KC_COMM,      KC_DOT,     KC_SLSH,    KC_ENT,
+        KC_LCTL,  KC_LGUI, KC_LALT, TG(2),      SQ_LOWER,       KC_TAB,       KC_SPC,       SQ_RAISE,     KC_LEFT,      KC_DOWN,    KC_UP,      KC_RGHT
     ),
     [1] = LAYOUT_ortho_4x12(    // Numbers and symbols
-        KC_GRV,   KC_1,    KC_2,    KC_3,       KC_4,           KC_5,          KC_6,      KC_7,         KC_8,         KC_9,       KC_0,       _______,
-        KC_COMM,  KC_DOT, KC_SLSH,  KC_SCLN,    KC_QUOT,        KC_ESC,        KC_BSPC,   KC_MINS,      KC_EQL,       KC_LBRC,    KC_RBRC,    KC_BSLS,
-        _______,  _______, _______, _______,    _______,        KC_TAB,        KC_ENT,    XXXXXXX,      KC_HOME,      KC_PGUP,    KC_PGDN,    KC_END,
-        KC_LCTL,  KC_LGUI, KC_LALT, KC_APP,     _______,        _______,       _______,    _______,     KC_LEFT,      KC_DOWN,    KC_UP,      KC_RGHT
+        KC_GRV,   KC_1,    KC_2,    KC_3,       KC_4,           KC_5,         KC_6,         KC_7,         KC_8,         KC_9,       KC_0,       _______,
+        KC_TAB,   XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,        KC_ESC,       KC_INS,       KC_MINS,      KC_EQL,       KC_LBRC,    KC_RBRC,    KC_BSLS,
+        _______,  _______, _______, _______,    _______,        XXXXXXX,      KC_DEL,       XXXXXXX,      KC_HOME,      KC_PGUP,    KC_PGDN,    KC_END,
+        _______,  _______, _______, KC_APP,     _______,        _______,      _______,      _______,      KC_LEFT,      KC_DOWN,    KC_UP,      KC_RGHT
     ),
-    [2] = LAYOUT_ortho_4x12(    // Function keys and  NumPad
-        KC_ESC,   KC_F1,   KC_F2,   KC_F3,      KC_F4,          KC_INS,        KC_DEL,    KC_NUM,       KC_P7,        KC_P8,      KC_P9,      KC_PSLS,
-        KC_TAB,   KC_F5,   KC_F6,   KC_F7,      KC_F8,          KC_ESC,        KC_BSPC,   KC_CAPS,      KC_P4,        KC_P5,      KC_P6,      KC_PAST,
-        KC_RSFT,  KC_F9,   KC_F10,  KC_F11,     KC_F12,         KC_TAB,        KC_ENT,    KC_SCRL,      KC_P1,        KC_P2,      KC_P3,      KC_PMNS,
-        KC_RCTL,  KC_RGUI, KC_RALT, _______,    XXXXXXX,        _______,       _______,   KC_APP,       KC_P0,        KC_PDOT,    KC_PENT,    KC_PPLS
+    [2] = LAYOUT_ortho_4x12(    // Function keys and NumPad
+        KC_ESC,   KC_F1,   KC_F2,   KC_F3,      KC_F4,          KC_CAPS,      KC_NUM,       KC_P7,        KC_P8,        KC_P9,      KC_PSLS,    KC_BSPC,
+        KC_TAB,   KC_F5,   KC_F6,   KC_F7,      KC_F8,          KC_SCRL,      KC_INS,       KC_P4,        KC_P5,        KC_P6,      KC_PAST,    S(KC_9),
+        KC_RSFT,  KC_F9,   KC_F10,  KC_F11,     KC_F12,         XXXXXXX,      KC_DEL,       KC_P1,        KC_P2,        KC_P3,      KC_PMNS,    S(KC_0),
+        KC_RCTL,  KC_RGUI, KC_RALT, _______,    _______,        _______,      _______,      KC_P0,        KC_PDOT,      KC_PENT,    KC_PPLS,    KC_EQL
     ),
     [3] = LAYOUT_ortho_4x12(    // RGB control, PrintScreen, Media keys
-        UG_HUED,  UG_HUEU, XXXXXXX, QK_BOOT,    XXXXXXX,        G(KC_PAUSE),   XXXXXXX,   LAG(KC_PSCR), A(KC_PSCR),   XXXXXXX,    XXXXXXX,    XXXXXXX,
-        UG_PREV,  UG_NEXT, UG_TOGG, XXXXXXX,    XXXXXXX,        _______,       XXXXXXX,   XXXXXXX,      XXXXXXX,      KC_MUTE,    XXXXXXX,    KC_MPLY,
-        XXXXXXX,  UG_VALU, XXXXXXX, XXXXXXX,    XXXXXXX,        _______,       XXXXXXX,   XXXXXXX,      XXXXXXX,      XXXXXXX,    KC_VOLU,    XXXXXXX,
-        UG_SPDD,  UG_VALD, UG_SPDU, XXXXXXX,    TO(0),          _______,       XXXXXXX,   XXXXXXX,      XXXXXXX,      KC_MPRV,    KC_VOLD,    KC_MNXT
+        UG_PREV,  UG_SPDU, UG_TOGG, XXXXXXX,    XXXXXXX,        QK_BOOT,      LAG(KC_PSCR), A(KC_PSCR),   XXXXXXX,      XXXXXXX,    XXXXXXX,    G(KC_PAUSE),
+        UG_NEXT,  UG_SPDD, UG_TOGG, XXXXXXX,    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,      XXXXXXX,    XXXXXXX,    XXXXXXX,
+        UG_HUEU,  UG_VALU, UG_SATU, XXXXXXX,    XXXXXXX,        XXXXXXX,      XXXXXXX,      XXXXXXX,      KC_MUTE,      XXXXXXX,    XXXXXXX,    KC_MPLY,
+        UG_HUED,  UG_VALD, UG_SATD, XXXXXXX,    TO(0),          _______,      _______,      XXXXXXX,      KC_MPRV,      KC_VOLD,    KC_VOLU,    KC_MNXT
     )
 };
+
+/*
+LED order
+Left              Right
+13,12,11,10,9,8,7,0,1,2,3,4,5,6
+*/
 
 const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {13, 1, HSV_WHITE}
@@ -58,16 +64,18 @@ const rgblight_segment_t PROGMEM my_numlock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
 );
 
 const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {8, 4, HSV_CYAN},
-    {1, 4, HSV_CYAN}
+    {9, 4, HSV_CYAN},
+    {3, 4, HSV_CYAN}
 );
 const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
-    {8, 4, HSV_PURPLE},
+    {9, 4, HSV_PURPLE},
     {1, 4, HSV_GREEN}
 );
 const rgblight_segment_t PROGMEM my_layer3_layer[] = RGBLIGHT_LAYER_SEGMENTS(
     {11, 3, HSV_ORANGE},
-    { 4, 3, HSV_BLUE}
+    { 7, 1, HSV_RED},
+    { 1, 2, HSV_GREEN},
+    { 3, 4, HSV_BLUE}
 );
 
 #define FIRST_LAYER 1
